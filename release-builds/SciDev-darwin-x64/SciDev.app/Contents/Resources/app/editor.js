072202034d06911c45df65aaa90fe2d96cbe8c9d
Marcus Edwards <marcusedwards@hotmail.ca>
2017-12-08 11:23:24 -0500

    repackage

---
+++ release-builds/SciDev-darwin-x64/SciDev.app/Contents/Resources/app/editor.js
@@ -0,0 +1,25 @@
+/**
+ * Created by marcusedwards on 2017-12-08.
+ */
+$(document).ready(function() {
+    $('.result').html($('#code-editor').text());
+    hljs.highlightBlock($('.result')[0],'  ', false);
+    $(document).on('keyup', '#code-editor', function(event) {
+        $('.result').html(this.innerText);
+        hljs.highlightBlock($('.result')[0],'  ', false);
+    });
+    $('.result').css({
+        position: "absolute",
+        top: $('#code-editor').position().top + "px",
+        left: ($('#code-editor').position().left)+ "px",
+        width: $('.editor').width() + 40
+    }).show();
+});
+$(window).resize(function(){
+    $('.result').css({
+        position: "absolute",
+        top: $('#code-editor').position().top + "px",
+        left: ($('#code-editor').position().left)+ "px",
+        width: $('.editor').width() + 40
+    });
+});
